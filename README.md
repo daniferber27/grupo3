fe0d5a18dd7c468f0a045c52590d08bc0891fd6d
LAV419 <lucasvellbach@outlook.com>
2024-06-24 19:39:44 -0300

    feat: update README.md

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # grupo3
 Trabajo Practico Final de la catedra de Inteligencia Computacional
+
+Notebook donde se entreno el modelo:
+https://colab.research.google.com/drive/1-_rYSXhxsg8pMzdRhweHJywgJ2rzdBTK?usp=sharing
+
+Base de datos:
+https://universe.roboflow.com/utech-susnq/license-plate-detection-wtqh2/dataset/1
--- Trabajo Integrador.py
+++ Trabajo Integrador.py
@@ -23,29 +23,21 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_heigth)    # Alto
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)      # Ancho
 
 # Exporta el modelo
-model = YOLO('yolov8n.pt')
-
-# Crea 'yolov8n_openvino_model/'
-model.export(format="openvino")  
-
-# Carga el modelo de OpenVINO
-ov_model = YOLO("yolov8n_openvino_model/")
-
-vehicle_classes = [2, 3, 5, 7] # 2 = car, 3 = motorcycle, 5 = bus, 7 = truck
+model = YOLO('best.onnx')
 
 while True:
         ret,frame = cap.read()
 
-        results = ov_model(frame)
+        results = model(frame)
 
         for result in results:
             for box in result.boxes:
-                if box.cls in vehicle_classes and box.conf > 0.30:  # Solo extrae los vehiculos detectados con una confianza superior al 30%
+                if box.conf > 0.30:  # Solo extrae los vehiculos detectados con una confianza superior al 30%
                     x1, y1, x2, y2 = box.xyxy[0].tolist()           # Extrae las coordenadas de la bounding box
                     conf = box.conf[0]                              # Extrae la confianza de la predicción
                     cls = box.cls[0]                                # Extrae la clase de la predicción
                     
-                    label = f'{ov_model.names[int(cls)]} {conf:.2f}'
+                    label = f'{model.names[int(cls)]} {conf:.2f}'
                     
                     # Dibuja la bounding box en el frame
                     cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 0), 2)
